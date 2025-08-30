35127a72efbae0e1d91ed3a184cbdec91bc72972
griffing52 <emailgriffinnow@gmail.com>
2025-08-30 07:27:48 +0000

    token names

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blockgen",
-    version="0.1",
-    packages=find_packages(where="blockgen"),
-    package_dir={"": "blockgen"},
+    version="0.1.0",
+    packages=find_packages(include=["blockgen", "blockgen.*"]),
+    python_requires=">=3.8",
 )
